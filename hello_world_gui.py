e97fd875de05f6559891bfba82f3a9fa380de4ef
Peter  Zhu <peter.zhu@ocsbstudent.ca>
2017-09-24 20:02:11 -0400

    Init

---
+++ hello_world_gui.py
@@ -0,0 +1,10 @@
+# Created by: Peter Zhu
+# Created on: 12-sep-2017
+# Created for: ICS3U
+# Daily Assignment - Unit0-02
+# This program dislays Hello World but in GUI
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
